dda912c9c4018c8caf25821210215436d88d4c06
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-11-23 22:31:16 +0300

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -4,6 +4,7 @@ mkdir data
 tar -xvf LJSpeech-1.1.tar.bz2 >> /dev/null
 mv LJSpeech-1.1 data/LJSpeech-1.1
 echo "done 1"
+rm LJSpeech-1.1.tar.bz2
 
 gdown https://drive.google.com/u/0/uc?id=1-EdH0t0loc6vPiuVtXdhsDtzygWNSNZx
 mv train.txt data/
@@ -12,16 +13,17 @@ echo "done 2"
 #download Waveglow
 gdown https://drive.google.com/u/0/uc?id=1WsibBTsuRg_SF2Z6L6NFRTT-NjEy1oTx
 mkdir -p waveglow/pretrained_model/
-mv waveglow_256channels_ljs_v2.pt waveglow/pretrained_model/waveglow_256channels.pt
+mv waveglow_256channels_ljs_v2.pt hw_tts/waveglow/pretrained_model/waveglow_256channels.pt
 echo "waveglow done"
 
 gdown https://drive.google.com/u/0/uc?id=1cJKJTmYd905a-9GFoo5gKjzhKjUVj83j
 tar -xvf mel.tar.gz
 echo $(ls mels | wc -l)
+rm mel.tar.gz
 
 #download alignments
 wget https://github.com/xcmyz/FastSpeech/raw/master/alignments.zip
 unzip alignments.zip >> /dev/null
 echo "alignments done"
-
+rm alignments.zip
 
