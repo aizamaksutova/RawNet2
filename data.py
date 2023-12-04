8839462eeaddf060e65567b29986ced52ebed546
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-12-04 23:59:01 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 import gdown
 
 url = "https://drive.google.com/u/0/uc?id=1-EdH0t0loc6vPiuVtXdhsDtzygWNSNZx"
-output = "train.txt"
+output = "checkpoint-epoch17.pth"
 gdown.download(url, output, quiet=False)
