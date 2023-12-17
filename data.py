d2589982a3f039572f980ffb407a91a4bc980975
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-12-18 00:06:02 +0300

    Update data.py

--- data.py
+++ data.py
@@ -2,5 +2,5 @@ import gdown
 
 # a checkpoint
 id_model = "1Y8VzCW1hyDLBmI41ze0SaZJppC16TgGk"
-output_model = "rawnet_model.pth"
+output_model = "model_rawnet.pth"
 gdown.download(id=id_model, output=output_model, quiet=False)
